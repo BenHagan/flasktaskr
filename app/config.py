71f2cb14de3a24c2cbda8f5334e159efe33e4820
textheadmin <textheadmin@users.noreply.github.com>
2015-01-21 01:46:36 -0600

    install sqlalchemy and port code and more wtform functionality

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ import os
 # grabs the folder where the script runs
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
@@ -13,3 +14,5 @@ SECRET_KEY = 'my_precious'
 
 # defines the full path for the DATABASE
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
