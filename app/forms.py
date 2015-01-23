0adca06ec72e2055a514c25bab1864839d3c6a7a
textheadmin <textheadmin@users.noreply.github.com>
2015-01-23 01:14:56 -0600

    changed validators from DataRequired to Input required per recommendation of WTForms

--- app/forms.py
+++ app/forms.py
@@ -3,18 +3,18 @@
 from flask_wtf import Form
 from wtforms import TextField, DateField, IntegerField, \
     SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo
 
 class AddTaskForm(Form):
     task_id = IntegerField('Priority')
-    name = TextField('Task Name', validators=[DataRequired()])
+    name = TextField('Task Name', validators=[InputRequired()])
     due_date = DateField(
         'Due Date (mm/dd/yyyy)',
-        validators=[DataRequired()], format='%m/%d/%Y'
+        validators=[InputRequired()], format='%m/%d/%Y'
         )
     priority = SelectField(
         'Priority',
-        validators=[DataRequired()],
+        validators=[InputRequired()],
         choices=[
             ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
             ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')
@@ -25,24 +25,24 @@ class AddTaskForm(Form):
 class RegisterForm(Form):
     name = TextField(
         'Username',
-        validators=[DataRequired(), Length(min=6, max=25)]
+        validators=[InputRequired(), Length(min=6, max=25)]
         )
     email = TextField(
         'Email',
-        validators=[DataRequired(),
+        validators=[InputRequired(),
         Length(min=6, max=40)]
         )
     password = PasswordField(
         'Password',
-        validators=[DataRequired(),
+        validators=[InputRequired(),
         Length(min=6, max=40)])
     confirm = PasswordField(
         'Repeat Password',
-        [DataRequired(),
+        [InputRequired(),
         EqualTo('password',
         message='Passwords must match')]
         )
 
 class LoginForm(Form):
-    name = TextField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    name = TextField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
