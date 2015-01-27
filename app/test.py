60928b37b4156fddce50925e188ab53ceb187c93
textheadmin <textheadmin@users.noreply.github.com>
2015-01-27 13:23:06 -0600

    add more tests

--- app/test.py
+++ app/test.py
@@ -26,6 +26,16 @@ class AllTests(unittest.TestCase):
     def tearDown(self):
         db.drop_all()
 
+    # login helper function
+    def login(self, name, password):
+        return self.app.post('/', data=dict(name=name, password=password),
+            follow_redirects=True)
+
+    def register(self, name, email, password, confirm):
+        return self.app.post('/register', data=dict(
+            name=name, email=email, password=password, confirm=confirm),
+            follow_redirects=True)
+
     # each test should start with 'test'
     def test_users_can_register(self):
         new_user = User("mherman", "michael@mherman.org", "michaelherman")
@@ -37,9 +47,32 @@ class AllTests(unittest.TestCase):
         assert t.name == 'mherman'
 
     def test_form_is_present_on_login_page(self):
+        #import pdb;pdb.set_trace()
         response = self.app.get('/')
         self.assertEquals(response.status_code, 200)
-        self.assertIn('Please sign in to access your task list', response.data)
+        self.assertIn('Please sign in to access your task list', 
+            response.get_data())
+
+    def test_users_cannot_login_unless_registered(self):
+        #import pdb;pdb.set_trace()
+        response = self.login('foo', 'bar')
+        self.assertIn('Invalid username or password', response.get_data())
+
+    def test_users_can_login(self):
+        self.register('Michael', 'michael@realpython.com', 'python', 'python')
+        response = self.login('Michael', 'python')
+        self.assertIn('You are logged in.  Go Crazy.', response.get_data())
+
+    def test_invalid_form_data(self):
+        self.register('Michael', 'michael@realpython.com', 'python', 'python')
+        response = self.login('alert("alert box!");', 'foo')
+        self.assertIn('Invalid username or password', response.get_data())
+
+    def test_form_is_present_on_register_page(self):
+        response = self.app.get('/register')
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('Please register to start a task list', 
+            response.get_data())
 
 if __name__ == "__main__":
     unittest.main()
