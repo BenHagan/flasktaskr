6ca1531c8194385fed09dc87f14240663251e53d
textheadmin <textheadmin@users.noreply.github.com>
2015-02-08 13:53:02 -0600

    install fab and make fabfile

---
+++ flasktaskr_project/fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+    with settings(warn_only=True)
+        result = local("python2 test_tasks.py -v && python2 test_users.py -v",
+                        capture=True)
+        if result.failed and not confirm("Tests failed. Continue?"):
+            abort("Aborted at user request.")
+
+def commit():
+    message = raw_input("Enter a commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
+
+def pull():
+    local("git pull origin master")
+
+def heroku():
+    local("git push heroku master")
+
+def heroku_test():
+    local("heroku run python test_tasks.py -v &&\
+         heroku run python test_users.py -v")
+
+def rollback():
+    local("heroku rollback")
+
+def prepare():
+    pull()
+    test()
+    commit()
+    heroku()
+    heroku_tests()
+    
+
